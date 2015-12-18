0680c59441a1664ac6667eb35621520b2dc65469
Johan Dahlberg <johan.dahlberg@medsci.uu.se>
2015-12-18 16:58:50 +0100

    Fixing docs

--- localq/JobPrioritizer.py
+++ localq/JobPrioritizer.py
@@ -1,11 +1,10 @@
 
-
 @staticmethod
 def fifo(pending_jobs, **kwargs):
     """
     Perform first in first out prioritizing
-    :param pending_jobs:
-    :return:
+    :param pending_jobs: list of jobs to prioritize
+    :return: the list of jobs sorted by priority
     """
     def priority_by_job_id(job):
         return -1 * job.jobid
@@ -13,11 +12,13 @@ def fifo(pending_jobs, **kwargs):
     return sorted(pending_jobs, key=priority_by_job_id, reverse=True)
 
 @staticmethod
-def max_nbr_cores_engaged(pending_jobs, **kwargs):
-    pass
-
-@staticmethod
 def max_cores_first(pending_jobs, **kwargs):
+    """
+    Run jobs with the maximum number of cores used first.
+    :param pending_jobs: list of jobs to prioritize
+    :param kwargs: list of jobs sorted by cores used.
+    :return:
+    """
     def priority_by_cores_used(job):
         return -1 * job.num_cores
     return sorted(pending_jobs, key=priority_by_cores_used, reverse=True)
